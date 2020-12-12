0355a30b475b1d2d27afdbf073cca1c0e08d5a62
Sławomir Jankowski <ostrokrzew@protonmail.com>
2020-12-12 14:16:43 +0100

    Add random number generator

---
+++ rng.h
@@ -0,0 +1,9 @@
+#ifndef ENGINEERPROJECT_RNG_H
+#define ENGINEERPROJECT_RNG_H
+
+#include "headers.h"
+
+uint8_t random_number_generator(std::string);
+uint8_t draw_number(const char*, int32_t&);
+
+#endif //ENGINEERPROJECT_RNG_H
